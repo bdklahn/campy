245ab2605e403c43f3a92db9d8bc3a5309b16450
bdklahn <brian.klahn@klahnpages.net>
2011-11-07 12:40:26 -0500

    This version actually works. It turns out I was on the right track about being able to call yield twice on two generator objects, in a new generator ("metagenerator") definition. What broke it was that the information ended up in the annotations field, instead of the name one.

--- reads-to-assembler.py
+++ reads-to-assembler.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 import screed, sys, string
 
-def is_pair(name1, name2):
-    if name1.endswith(' 1:N:0:TAGCTT\n') and name2.endswith(' 1:N:0:TAGCTT\n'):
+def is_pair(name1, annotations1, name2, annotations2):
+    if '1:N:0:' in annotations1 and '2:N:0:' in annotations2:
         s1 = name1.split(' ')[0]
         s2 = name2.split(' ')[0]
         if s1 == s2:
@@ -12,46 +12,65 @@ def is_pair(name1, name2):
 
 infile = sys.argv[1]
 outfile = infile.replace('R1_', '')
-outfile = infile.replace('fq', 'fa')
-
+outfile = outfile.replace('fq', 'fa')
+outfile = outfile.replace('.gz', '')
 reads = screed.open(infile)
 
-if len(sys.argv) > 2:
-   reads_fwd = screed.open(sys.argv[2])
-   def alternate():
-      for rec in reads:
-         yield(rec)
-         yield(reads)
-   reads = alternate()
+num_args = len(sys.argv)
+
+if num_args == 1:
+  print('You\'ll need some read sequences files for this.')
+  exit()
+elif num_args == 2:
+  try:
+    reads = screed.open(sys.argv[1])
+  except:
+    print('argument doesn\'t appear to be an existing fasta/q filepath')
+    exit()
+else: 
+  try:
+    reads_f1 = screed.open(sys.argv[1])
+    reads_f2 = screed.open(sys.argv[2])
+  except:
+    print('An argument doesn\'t appear to be an existing fasta/q filepath')
+    exit()
+  def alternate():
+    for n in reads_f1:
+      yield(n)
+      yield(reads_f2.next())
+  reads = alternate()
 
 single_fp = open(outfile + '.se', 'w')
 paired_fp = open(outfile + '.pe', 'w')
 
 last_record = None
 last_name = None
+last_annotations = None
 for record in reads:
     name = record['name'].split()[0]
+    annotations = record['annotations']
     sequence = record['sequence']
 
     if last_record:
-        if is_pair(last_name, name):
+        if is_pair(last_name, last_annotations, name, annotations):
            print >>paired_fp, '>%s\n%s' % (last_name, last_record['sequence'])
            print >>paired_fp, '>%s\n%s' % (name, record['sequence'])
-           name, record = None, None
+           name, annotations, record = None, None, None
         else:
            print >>single_fp, '>%s\n%s' % (last_name, last_record['sequence'])
 
     last_name = name
+    last_annotations = annotations
     last_record = record
 
 if last_record:
-    if is_pair(last_name, name):
+    if is_pair(last_name, last_annotations, name, annotations):
         print >>paired_fp, '>%s\n%s' % (last_name, last_record['sequence'])
         print >>paired_fp, '>%s\n%s' % (name, record['sequence'])
-        name, record = None, None
+        name, annotations, record = None, None, None
     else:
         print >>single_fp, '>%s\n%s' % (last_name, last_record['sequence'])
-        name, record = None, None
+        name, annotations, record = None, None, None
 
 if record:
    print >>single_fp, '>%s\n%s' % (name, record['sequence'])
